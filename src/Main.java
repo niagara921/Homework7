934276e9a3cb13122b6184b5239a152bacdf7968
Татьяна <niagara921@mail.ru>
2022-09-02 03:37:04 +0500

    Домашнее задание 7

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
+        String firstName = "Ivan ";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov ";
+        String fullName = lastName + firstName + middleName;
+        System.out.println("ФИО сотрудника —  " + fullName );
+
+        // Задача 2
+
+        System.out.println("Данные ФИО сотрудника для заполнения отчета —  " + fullName.toUpperCase());
+
+        // Задача 3
+
+        fullName = "Иванов Семён Семёнович";
+        System.out.println(fullName.replace("ё","е"));
+
+
 
     }
 }
